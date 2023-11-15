d41e08b8a7b69be9b84f4f31a91514827472d77e
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-15 21:22:55 +0300

    Kullanıcı tarafından girilen tam sayıyı yazdıran C programı

---
+++ ikinci.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//Kullanıcı tarafından girilen tam sayıyı yazdıran C programı
+int main( void ){
+	
+	int sayi;
+	printf("sayi:");
+	scanf("%d",&sayi);
+	printf("girilen deger: %d",sayi);
+	
+}
