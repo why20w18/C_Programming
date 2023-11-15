b796772f8b8088b208c5e8b7d4396340a07cb7c2
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-15 21:23:57 +0300

    kullanici tarafindan girilen karakterin ascii değerini bulan C programı

---
+++ besinci.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//kullanici tarafindan girilen karakterin ascii değerini bulan C programı
+int main( void ){
+	char karakter;
+	printf("karakter:");
+	scanf("%c",&karakter);
+	printf("ascii: %d",karakter);
+}
