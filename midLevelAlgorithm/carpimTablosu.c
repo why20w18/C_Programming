bf99e45df6587d2d34722a74e2b7163bf4e9e2ba
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-16 18:29:36 +0300

    Create carpimTablosu.c

---
+++ midLevelAlgorithm/carpimTablosu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//çarpım tablosu oluşturan C programı
+int main(){
+	int i , j;
+	for(i = 1 ; i <= 10 ; i++){
+		for(j = 1 ; j <= 10 ; j++){
+			
+			printf("%d x %d = %d\n",i,j,(i*j));
+			
+		}
+		printf("\n");
+	}
+	
+}
