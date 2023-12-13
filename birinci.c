b1fe8ae123d34323dc71c1758eae6b544974c68e
bug <147606593+why20w18@users.noreply.github.com>
2023-12-13 03:40:02 +0300

    Delete birinci.c

--- birinci.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-//cümleyi ekrana yazdıran C programı
-int main( void ){
-	
-	char string[100];
-	printf("kelime:");
-	fgets(string , 100 , stdin); //scanf("[^\n]%d",string);
-	printf("%s",string);
-}
