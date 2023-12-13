a0abe31b5c155a1a40559c7252513d7cb50c4e05
bug <147606593+why20w18@users.noreply.github.com>
2023-12-13 03:39:53 +0300

    Delete besinci.c

--- besinci.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-//kullanici tarafindan girilen karakterin ascii değerini bulan C programı
-int main( void ){
-	char karakter;
-	printf("karakter:");
-	scanf("%c",&karakter);
-	printf("ascii: %d",karakter);
-}
