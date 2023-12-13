1973cc135b0e6553060e93ec3f14389218dcd81e
bug <147606593+why20w18@users.noreply.github.com>
2023-12-13 03:40:45 +0300

    Delete ucuncu.c

--- ucuncu.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-//Kullanıcı tarafından girilen iki tam sayiyi toplayan C programı
-int main( void ){
-	
-	int a,b;
-	printf("iki sayi gir\n");
-	scanf("%d %d",&a,&b);
-	printf("sonuc: %d",(a+b));
-}
