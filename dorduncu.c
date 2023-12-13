86595edbc70b0827df288480e55ba579afc89ed4
bug <147606593+why20w18@users.noreply.github.com>
2023-12-13 03:40:20 +0300

    Delete dorduncu.c

--- dorduncu.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-//iki ondalıklı sayiyi çarpma işlemi yapan C programı
-int main( void ){
-	double x,y;
-	printf("2 ondalikli sayi gir (ornek:4.5 x 6.1)\n");
-	scanf("%lf %lf",&x,&y);
-	printf("sonuc: %.2lf",(x*y));
-	
-}
